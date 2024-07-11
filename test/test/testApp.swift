fbddcd7fe4fae2ab540ddf7d5a892c9a593b3857
Ricoh <ricoh@willog.io>
2024-07-11 10:05:56 +0900

    asdf

---
+++ test/test/testApp.swift
@@ -0,0 +1,17 @@
+//
+//  testApp.swift
+//  test
+//
+//  Created by Ricoh Jeong on 7/11/24.
+//
+
+import SwiftUI
+
+@main
+struct testApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
